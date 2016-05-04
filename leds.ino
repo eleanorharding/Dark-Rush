3807b13424cbd3eba870acfff1dea2bbdee1ba7b
Leonard Silverberg <leosilverberg@gmail.com>
2016-05-04 17:51:10 +0100

    Blinking LEDs

---
+++ leds.ino
@@ -0,0 +1,238 @@
+int y1 = 22;
+int y2 = 26;
+int y3 = 30;
+int y4 = 34;
+int y5 = 38;
+int y6 = 42;
+
+int r1 = 24;
+int r2 = 28;
+int r3 = 32;
+int r4 = 36;
+int r5 = 40;
+int r6 = 44;
+
+void setup() {
+
+  pinMode(y1, OUTPUT);
+  pinMode(y2, OUTPUT);
+  pinMode(y3, OUTPUT);
+  pinMode(y4, OUTPUT);
+  pinMode(y5, OUTPUT);
+  pinMode(y6, OUTPUT);
+
+  pinMode(r1, OUTPUT);
+  pinMode(r2, OUTPUT);
+  pinMode(r3, OUTPUT);
+  pinMode(r4, OUTPUT);
+  pinMode(r5, OUTPUT);
+  pinMode(r6, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // YELLOW
+  digitalWrite(y1, HIGH);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+//RED
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, HIGH);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//YELLOW
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, HIGH);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//RED
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, HIGH);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//YELLOW
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, HIGH);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//RED
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, HIGH);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//YELLOW
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, HIGH);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//RED
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, HIGH);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//YELLOW
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, HIGH);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//RED
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, HIGH);
+  digitalWrite(r6, LOW);
+  delay(20);
+
+
+//YELLOW
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, HIGH);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, LOW);
+  delay(20);
+//RED
+   digitalWrite(y1, LOW);   
+  digitalWrite(y2, LOW);
+  digitalWrite(y3, LOW);
+  digitalWrite(y4, LOW);
+  digitalWrite(y5, LOW);
+  digitalWrite(y6, LOW);
+
+  digitalWrite(r1, LOW);
+  digitalWrite(r2, LOW);
+  digitalWrite(r3, LOW);
+  digitalWrite(r4, LOW);
+  digitalWrite(r5, LOW);
+  digitalWrite(r6, HIGH);
+  delay(20);
+  
+  
+  
+ 
+  
+}
